0382e3fc927f666d42fc850bee35d0f1230d4e03
Louise Whitaker <louisewhitaker@louises-imac.lan>
2017-08-20 23:16:54 -0700

    Added portfolio page

--- js/main.js
+++ js/main.js
@@ -42,7 +42,7 @@ jQuery(document).ready(function($){
 			});
 		}
 	}
-  
+
   function updateNavBar() {
     var headroom = nav.data('headroom');
     headroom.offset = nav.offset().top + 40;
@@ -92,3 +92,173 @@ $('#back-to-top').on('click', function (e) {
         scrollTop: 0
     }, 700);
 });
+
+// Process Page
+
+(function($) {
+
+	$(document).ready(function() {
+		setupFade();
+		setupClickToScroll();
+		setupPostAnimation();
+
+    enableScrollAbortion();
+
+		// Trigger window.scroll, this will initiate some of the scripts
+		$(window).scroll();
+  });
+
+
+  // Allow user to cancel scroll animation by manually scrolling
+  function enableScrollAbortion() {
+    var $viewport = $('html, body');
+    $viewport.on('scroll mousedown DOMMouseScroll mousewheel keyup', function(e) {
+        if ( e.which > 0 || e.type === 'mousedown' || e.type === 'mousewheel') {
+             $viewport.stop();
+        }
+    });
+  }
+
+	function setupPostAnimation() {
+		var posts = $('.post-wrapper .post');
+		$(window).on('scroll resize', function() {
+
+			var currScroll = $(window).scrollTop() > $(document).scrollTop() ? $(window).scrollTop() : $(document).scrollTop(),
+				windowHeight = $(window).height(), // Needs to be here because window can resize
+				overScroll = Math.ceil(windowHeight*.20),
+				treshhold = (currScroll + windowHeight) - overScroll;
+
+			posts.each(function() {
+
+				var post = $(this),
+					postScroll = post.offset().top
+
+				if(postScroll > treshhold) {
+					post.addClass('hidden');
+				} else {
+					post.removeClass('hidden');
+				}
+
+			});
+
+		});
+	}
+
+	function setupFade() {
+
+		var posts = $('.post-wrapper .post').reverse(),
+			stemWrapper = $('.stem-wrapper'),
+			halfScreen = $(window).height() / 2;
+
+		$(window).on('scroll resize', function() {
+
+			delay(function() {
+
+				var currScroll = $(window).scrollTop() > $(document).scrollTop() ? $(window).scrollTop() : $(document).scrollTop(),
+					scrollSplit = currScroll + halfScreen;
+
+				posts.removeClass('active').each(function() {
+
+					var post = $(this),
+						postOffset = post.offset().top;
+
+					if(scrollSplit > postOffset) {
+
+						// Add active class to fade in
+						post.addClass('active')
+
+						// Get post color
+						var color = post.data('stem-color') ? post.data('stem-color') : null,
+							allColors = 'color-pink color-orange'
+
+						stemWrapper.removeClass(allColors);
+
+						if(color !== null) {
+							stemWrapper.addClass('color-' + color);
+						}
+
+						return false;
+					}
+
+				});
+			}, 20);
+
+		});
+
+	}
+
+
+	function setupClickToScroll(post) {
+
+		var scrollSpeed = 750;
+
+		$('.post-wrapper .post .stem-overlay .icon').click(function(e) {
+			e.preventDefault();
+
+			var icon = $(this),
+				post = icon.closest('.post'),
+				postTopOffset = post.offset().top,
+				postHeight = post.height(),
+				halfScreen = $(window).height() / 2,
+				scrollTo = postTopOffset - halfScreen + (postHeight/2);
+
+			$('html, body').animate({
+				scrollTop: scrollTo
+			}, scrollSpeed);
+		});
+
+	}
+
+})(jQuery);
+
+
+
+
+/*==========  Helpers  ==========*/
+
+
+// Timeout function
+var delay = (function(){
+	var timer = 0;
+	return function(callback, ms){
+		clearTimeout (timer);
+		timer = setTimeout(callback, ms);
+	};
+})();
+
+$.fn.reverse = function() {
+    return this.pushStack(this.get().reverse(), arguments);
+};
+
+// End of Process Page
+
+//Resume Page
+
+jQuery(document).ready(function($){
+	var timelineBlocks = $('.timeline-block'),
+		offset = 0.8;
+
+	//hide timeline blocks which are outside the viewport
+	hideBlocks(timelineBlocks, offset);
+
+	//on scolling, show/animate timeline blocks when enter the viewport
+	$(window).on('scroll', function(){
+		(!window.requestAnimationFrame)
+			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+	});
+
+	function hideBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.timeline-img, .timeline-content').addClass('is-hidden');
+		});
+	}
+
+	function showBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.timeline-img').hasClass('is-hidden') ) && $(this).find('.timeline-img, .timeline-content').removeClass('is-hidden').addClass('bounce-in');
+		});
+	}
+});
+
+//END OF RESUME PAGE
