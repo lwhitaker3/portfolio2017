993f0a0f0d66b55e51ba56e7ef24f58f0d573de2
Louise Whitaker <louisewhitaker@louises-imac.lan>
2017-08-22 17:28:38 -0700

    Added Favicon

--- js/main.js
+++ js/main.js
@@ -1,70 +1,70 @@
-// jQuery(document).ready(function($){
-// 	var introSection = $('#jumbotron-content'),
-// 		introSectionHeight = introSection.height(),
-// 		//change scaleSpeed if you want to change the speed of the scale effect
-// 		scaleSpeed = 0.3,
-// 		//change opacitySpeed if you want to change the speed of opacity reduction effect
-// 		opacitySpeed = 1;
-//
-// 	//update this value if you change this breakpoint in the style.css file (or _layout.scss if you use SASS)
-// 	var MQ = 1170;
-//
-// 	triggerAnimation();
-// 	$(window).on('resize', function(){
-// 		triggerAnimation();
-//     updateNavBar();
-// 	});
-//
-// 	//bind the scale event to window scroll if window width > $MQ (unbind it otherwise)
-// 	function triggerAnimation(){
-// 		if($(window).width()>= MQ) {
-// 			$(window).on('scroll', function(){
-// 				//The window.requestAnimationFrame() method tells the browser that you wish to perform an animation- the browser can optimize it so animations will be smoother
-// 				window.requestAnimationFrame(animateIntro);
-// 			});
-// 		} else {
-// 			$(window).off('scroll');
-// 		}
-// 	}
-// 	//assign a scale transformation to the introSection element and reduce its opacity
-// 	function animateIntro () {
-// 		var scrollPercentage = ($(window).scrollTop()/introSectionHeight).toFixed(5),
-// 			scaleValue = 1 - scrollPercentage*scaleSpeed;
-// 		//check if the introSection is still visible
-// 		if( $(window).scrollTop() < introSectionHeight) {
-// 			introSection.css({
-// 			    '-moz-transform': 'scale(' + scaleValue + ') translateZ(0)',
-// 			    '-webkit-transform': 'scale(' + scaleValue + ') translateZ(0)',
-// 				'-ms-transform': 'scale(' + scaleValue + ') translateZ(0)',
-// 				'-o-transform': 'scale(' + scaleValue + ') translateZ(0)',
-// 				'transform': 'scale(' + scaleValue + ') translateZ(0)',
-// 				'opacity': 1 - scrollPercentage*opacitySpeed
-// 			});
-// 		}
-// 	}
-//
-//   function updateNavBar() {
-//     var headroom = nav.data('headroom');
-//     headroom.offset = nav.offset().top + 40;
-//     headroom.update();
-//     console.log("Updated: " + headroom.offset);
-//   }
-//   window.nav = $("#main-nav");
-//   nav.headroom({
-//     // Offset set in updateNavBar.
-//     "tolerance": 5,
-//     "classes": {
-//       "initial": "animated",
-//       "pinned": "slideDown",
-//       "unpinned": "slideUp"
-//     }
-//   });
-//   navSticky = new Waypoint.Sticky({
-//     element: nav[0],
-//     stuckClass: 'fixed-top',
-//   });
-//   updateNavBar();
-// });
+jQuery(document).ready(function($){
+	var introSection = $('#jumbotron-content'),
+		introSectionHeight = introSection.height(),
+		//change scaleSpeed if you want to change the speed of the scale effect
+		scaleSpeed = 0.3,
+		//change opacitySpeed if you want to change the speed of opacity reduction effect
+		opacitySpeed = 1;
+
+	//update this value if you change this breakpoint in the style.css file (or _layout.scss if you use SASS)
+	var MQ = 1170;
+
+	triggerAnimation();
+	$(window).on('resize', function(){
+		triggerAnimation();
+    updateNavBar();
+	});
+
+	//bind the scale event to window scroll if window width > $MQ (unbind it otherwise)
+	function triggerAnimation(){
+		if($(window).width()>= MQ) {
+			$(window).on('scroll', function(){
+				//The window.requestAnimationFrame() method tells the browser that you wish to perform an animation- the browser can optimize it so animations will be smoother
+				window.requestAnimationFrame(animateIntro);
+			});
+		} else {
+			$(window).off('scroll');
+		}
+	}
+	//assign a scale transformation to the introSection element and reduce its opacity
+	function animateIntro () {
+		var scrollPercentage = ($(window).scrollTop()/introSectionHeight).toFixed(5),
+			scaleValue = 1 - scrollPercentage*scaleSpeed;
+		//check if the introSection is still visible
+		if( $(window).scrollTop() < introSectionHeight) {
+			introSection.css({
+			    '-moz-transform': 'scale(' + scaleValue + ') translateZ(0)',
+			    '-webkit-transform': 'scale(' + scaleValue + ') translateZ(0)',
+				'-ms-transform': 'scale(' + scaleValue + ') translateZ(0)',
+				'-o-transform': 'scale(' + scaleValue + ') translateZ(0)',
+				'transform': 'scale(' + scaleValue + ') translateZ(0)',
+				'opacity': 1 - scrollPercentage*opacitySpeed
+			});
+		}
+	}
+
+  function updateNavBar() {
+    var headroom = nav.data('headroom');
+    headroom.offset = nav.offset().top + 40;
+    headroom.update();
+    console.log("Updated: " + headroom.offset);
+  }
+  window.nav = $("#main-nav");
+  nav.headroom({
+    // Offset set in updateNavBar.
+    "tolerance": 5,
+    "classes": {
+      "initial": "animated",
+      "pinned": "slideDown",
+      "unpinned": "slideUp"
+    }
+  });
+  navSticky = new Waypoint.Sticky({
+    element: nav[0],
+    stuckClass: 'fixed-top',
+  });
+  updateNavBar();
+});
 
 $("#main-nav ul li a[href^='#']").on('click', function(e) {
 
